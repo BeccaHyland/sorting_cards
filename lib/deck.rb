be8afd4385917a83c68f7239878fb116525a06f9
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-07-04 21:15:29 -0600

    updates sort test and drafts bubble sort

--- lib/deck.rb
+++ lib/deck.rb
@@ -13,4 +13,16 @@ class Deck
     @cards.count
   end
 
+  def sort(deck)
+    deck_length = deck.length
+    loop do
+        swapped = false
+        (deck_length-1).times do |thing|
+          if deck |thing| > deck |thing + 1|
+
+        end
+      end
+    end
+  end
+
 end
